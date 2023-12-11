c4be9117d61444cab27af6611ccdfcc0bdfefe3a
Thanh tam <lentanta@pop-os.localdomain>
2023-12-11 20:54:29 +0700

    Improve random walker

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
-import './style.css';
 import P5 from "p5";
 
+class Walker {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  moveX(x: number) {
+    this.x = this.x + x;
+  }
+
+  moveY(y: number) {
+    this.y = this.y + y;
+  }
+}
+
 const sketch = (p5: P5) => {
-  let x = 500 / 2;
-  let y = 500 / 2;
+  const width = 500;
+  const height = 500;
 
-  let x1 = 500 / 2;
-  let y1 = 500 / 2;
-
-  let up = 0;
-  let right = 0;
-  let left = 0;
-  let down = 0;
+  let walker = new Walker(width / 2, height / 2);
+  const walkerSize = 10;
 
   p5.setup = () => {
     p5.createCanvas(500, 500)
@@ -19,43 +31,37 @@ const sketch = (p5: P5) => {
   }
 
   p5.draw = () => {
-    p5.background('rgba(0%,0%,0%,1)');
-
-    p5.fill(255);
-    p5.text(`UP:${up}\n DOWN:${down}\n LEFT:${left}\n RIGHT:${right}`, 0, 10);
-    p5.text(`${Math.floor(p5.noise(p5.frameCount) * 10)}`, 0, 100);
-    console.log(Math.floor(p5.sin(p5.frameCount) * 10))
+    p5.background('rgba(0%,0%,0%,0.05)');
 
     p5.noStroke();
-    p5.square(x, y, 10);
+    p5.square(
+      walker.x,
+      walker.y,
+      walkerSize
+    );
     const rndNumber = Math.floor(p5.random(0, 4))
 
     switch (rndNumber) {
       case 0:
-        left += 1
-        if (x > 10) {
-          x -= 10
-        }
+        if (walker.x > walkerSize)
+          walker.moveX(-12)
+
         break;
       case 1:
-        right += 1
-        if (x < 500 - 10) {
-          x += 10
-        }
+        if (walker.x < width - walkerSize)
+          walker.moveX(12)
+
         break;
       case 2:
-        down += 1
-        if (y < 500 - 10) {
-          y += 10
-        }
+        if (walker.y < width - walkerSize)
+          walker.moveY(12)
+
         break;
       case 3:
-        up += 1
-        if (y > 10) {
-          y -= 10
-        }
-        break;
+        if (walker.y > walkerSize)
+          walker.moveY(-12)
 
+        break;
     }
   }
 }
